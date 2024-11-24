b23b4bd08bfba22585e884e04892bf8fe7a8e33d
KeithRussel <keithmaniacop@gmail.com>
2024-11-24 19:02:21 +0800

    create API service using Axios

---
+++ src/services/api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://api.openweathermap.org/data/2.5',
+});
+
+export const fetchWeather = async (city: string, unit: 'metric' | 'imperial') => {
+    const response = await api.get('/weather', {
+        params: {
+            q: city,
+            units: unit,
+            appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+        }
+    });
+    return response.data;
+}
+
+export const fetchForecast = async (city: string, unit: 'metric' | 'imperial') => {
+    const response = await api.get('/forecast', {
+        params: {
+            q: city,
+            units: unit,
+            appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+        }
+    });
+    return response.data;
+}
+
+export default api;
