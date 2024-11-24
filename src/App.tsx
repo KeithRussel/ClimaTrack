155c13a0647ab7191e05b0677e9a7480009313f9
KeithRussel <keithmaniacop@gmail.com>
2024-11-24 23:42:10 +0800

    Apply Forecast Component on UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import CurrentWeather from './components/CurrentWeather';
 import ErrorBoundary from './components/ErrorBoundary';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <ErrorBoundary>
       <CurrentWeather />
+      <Forecast />
     </ErrorBoundary>
   );
 }
