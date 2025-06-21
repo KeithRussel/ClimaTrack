871ef387dfa942fa3179482f96abbcd081de4c7b
KeithRussel <keithmaniacop@gmail.com>
2025-06-21 17:29:36 +0800

    Refactor Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,14 @@
-import CurrentWeather from '../components/CurrentWeather';
-import Forecast from '../components/Forecast';
+import CurrentWeather from '../components/CurrentWeather'; // Component to display current weather
+import Forecast from '../components/Forecast'; // Component to display weather forecast
+import WeatherSearch from '../components/WeatherSearch'; // Component for city search input
 
 const Homepage = () => {
   return (
     <>
-      <div>
-        <CurrentWeather />
-      </div>
-      <div>{/* <Forecast /> */}</div>
+      <h1>ClimaTrack</h1> {/* A main title for your app */}
+      <WeatherSearch /> {/* The search input for the city */}
+      <CurrentWeather /> {/* Displays the current weather data */}
+      <Forecast /> {/* Displays the weather forecast data */}
     </>
   );
 };
