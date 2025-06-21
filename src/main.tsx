fc747cd183a30ef22806aa05c7b47450e9a631c5
KeithRussel <keithmaniacop@gmail.com>
2025-06-21 17:27:45 +0800

    Refactor main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
-import router from './routes';
+import router from './routes'; // Your defined routes
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // For debugging data fetches
+import './index.css'; // Global styles
 
+// Create a client for React Query. This manages data fetching, caching, and more.
 const queryClient = new QueryClient();
 
+// Render your React application into the 'root' element in index.html
 ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(
+  // StrictMode helps in identifying potential problems in an application
   <React.StrictMode>
+    {/* QueryClientProvider makes the queryClient available to all components */}
     <QueryClientProvider client={queryClient}>
-      {/* <App /> */}
+      {/* RouterProvider manages navigation based on your defined routes */}
       <RouterProvider router={router} />
+      {/* ReactQueryDevtools is a powerful debugging tool for React Query */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
